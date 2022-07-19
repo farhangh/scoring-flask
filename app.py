8a95b6c821ce5669b2f42f0ade7d5afd2cf234b1
Farhang Habibi <17721302+farhangh@users.noreply.github.com>
2022-07-19 14:45:43 +0200

    test3

--- app.py
+++ app.py
@@ -2,18 +2,22 @@ import pandas as pd
 from flask import Flask, request, jsonify
 from p7scoring.feature_extraction import get_features
 from p7scoring.models import read_data, load_model
+
+from sklearn.preprocessing import StandardScaler
+
 import json
 
 #data = read_data(path="data/p7_data.csv")
 data = read_data()
 features = get_features(data)
+
 data = data[features+ ["SK_ID_CURR", "TARGET"]].copy()
-data = data.reset_index(drop=True)
+
+X = data[features].values
+X_sc = StandardScaler().fit_transform(X)
 
 model = load_model()
 
-#score = model.predict_proba(data.loc[data['SK_ID_CURR']==100700, features].values)
-#print(score)
 
 # instantiate Flask object
 app = Flask(__name__)
@@ -27,15 +31,13 @@ def index():
 @app.route('/id_score/')
 def get_score():
     id = int(request.args.get('SK_ID_CURR'))
-    id_data = data.loc[data['SK_ID_CURR']==id, features].values
+    id_data = data[data['SK_ID_CURR']==id]
+    idx = id_data.index
     if(id_data.shape[0]==0):
         score = pd.DataFrame(data=[-1], columns=["score"])
     else:
-        score = pd.DataFrame(data=[int(model.predict(id_data)[0])], columns=["score"])
-        #print(model.predict_proba(id_data)[0])
-        #print(model.predict(id_data)[0])
-        #print(data.loc[data['SK_ID_CURR']==id, "TARGET"].values)
-
+        score = pd.DataFrame(data=[int(model.predict(X_sc[idx])[0])], columns=["score"])
+  
     return jsonify( json.loads(score.to_json()) )
 
 # Putting ids in dictionary (json file)
