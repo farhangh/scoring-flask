590f38d4fc215dadc8491d5a5bd3248eb88ca835
Farhang Habibi <17721302+farhangh@users.noreply.github.com>
2023-04-05 10:00:36 +0200

    test.py pytest first decorator activated 5

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import pytest
-import app
+from app import app
 import json
 
 test_data = [
@@ -8,23 +8,21 @@ test_data = [
     ("204", {'0': -1}),
 ]
 
-def test_index():
-    assert (app.index() == "App, model and data loaded ...")
+#def test_index():
+#    assert (app.index() == "App, model and data loaded ...")
 
 @pytest.mark.parametrize("SK_ID_CURR, expected", test_data)
 def test_get_score(SK_ID_CURR, expected):
-    response = app.app.test_client().get(f'/id_score/?SK_ID_CURR={SK_ID_CURR}')
+    response = app.test_client().get(f'/id_score/?SK_ID_CURR={SK_ID_CURR}')
     res = json.loads(response.data.decode('utf-8')).get("score")
     assert(res==expected)
-"""
-@pytest.mark.parametrize("n")
-def test_get_features(n):
-    response = app.app.test_client().get(f'/features/?n={n}')
-    res = json.loads(response.data.decode('utf-8')).get("n")
-    assert (res == {"0":0,"1":1})
-"""
 
-#if __name__ == "__main__":
-#    test_index()
-#    test_get_score("204",.9)
-#    test_get_features(2)
+
+test_n_data=[ (1,{"0":0}), (2, {"0":0,"1":1}) ]
+@pytest.mark.parametrize("n, expected", test_n_data)
+def test_get_features(n, expected):
+    response = app.test_client().get(f'/features/?n={n}')
+    res = json.loads(response.data.decode('utf-8')).get("n")
+    assert (res == expected)
+
+
