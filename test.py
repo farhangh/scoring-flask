177e6be839e0bfc3854476443d667b2b49d73461
Farhang Habibi <17721302+farhangh@users.noreply.github.com>
2023-04-05 09:51:21 +0200

    test.py pytest first decorator activated 3

--- test.py
+++ test.py
@@ -2,16 +2,22 @@ import pytest
 import app
 import json
 
+test_data = [
+    ("100700",.5),
+    ("100703",.6),
+    ("204",.9),
+    ("151",.7),
+    ("6734",.2)
+]
+
 def test_index():
     assert (app.index() == "App, model and data loaded ...")
 
-@pytest.mark.parametrize("SK_ID_CURR")
-def test_get_score(SK_ID_CURR):
-    print("Hello javoun", SK_ID_CURR)
-    #response = app.app.test_client().get(f'/id_score/?SK_ID_CURR={SK_ID_CURR}')
-    #res = json.loads(response.data.decode('utf-8')).get("score")
-    #assert(res=={"0":-1})
-    #print(res)
+@pytest.mark.parametrize("SK_ID_CURR, expected", test_data)
+def test_get_score(SK_ID_CURR, expected):
+    response = app.app.test_client().get(f'/id_score/?SK_ID_CURR={SK_ID_CURR}')
+    res = json.loads(response.data.decode('utf-8')).get("score")
+    assert(res==expected)
 """
 @pytest.mark.parametrize("n")
 def test_get_features(n):
@@ -22,5 +28,5 @@ def test_get_features(n):
 
 if __name__ == "__main__":
     test_index()
-    test_get_score(1)
+    test_get_score("204",.9)
 #    test_get_features(2)
