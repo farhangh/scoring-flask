caec1ff11063975d302fcdc57772c78da8dff228
Farhang Habibi <17721302+farhangh@users.noreply.github.com>
2023-08-05 15:26:40 +0200

    test.py modified

--- test.py
+++ test.py
@@ -2,24 +2,33 @@ import pytest
 from app import app, index
 import json
 
-test_data = [
-    ("100700",{'0': 0}),
-    ("100703",{'0': 0}),
-    ("204", {'0': -1}),
-]
+
 
 def test_index():
     assert (index() == "App, model and data loaded ...")
 
+
+
+test_data = [
+    ("100700",0),
+    ("100703",0),
+    ("204", -1),
+]
 @pytest.mark.parametrize("SK_ID_CURR, expected", test_data)
 def test_get_score(SK_ID_CURR, expected):
-    response = app.test_client().get(f'/id_score/?SK_ID_CURR={SK_ID_CURR}')
+    response = app.test_client().get(f'/id_score/?id={SK_ID_CURR}')
     res = json.loads(response.data.decode('utf-8')).get("score")
     assert(res==expected)
 
 
-test_n_data=[ (1,{"0":0}), (2, {"0":0,"1":1}) ]
-@pytest.mark.parametrize("n, expected", test_n_data)
+
+test_featues = [
+    ("1", ["AMT_CREDIT"]),
+    ("3", ["AMT_CREDIT","DAYS_BIRTH","NAME_EDUCATION_TYPE_Secondary / secondary special"]),
+    ("5", ["AMT_CREDIT","DAYS_BIRTH","NAME_EDUCATION_TYPE_Secondary / secondary special",
+         "AMT_ANNUITY","REGION_RATING_CLIENT_W_CITY"]) ]
+
+@pytest.mark.parametrize("n, expected", test_featues)
 def test_get_features(n, expected):
     response = app.test_client().get(f'/features/?n={n}')
     res = json.loads(response.data.decode('utf-8')).get("n")
